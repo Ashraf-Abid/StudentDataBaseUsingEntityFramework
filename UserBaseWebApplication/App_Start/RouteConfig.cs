bdea0093dfdf3e5afef41a75cedfbb71192049a8
Ashraf-Abid <abid7ctg@gmail.com>
2021-10-11 22:45:14 +0600

    User Base Login Logout Added

--- UserBaseWebApplication/App_Start/RouteConfig.cs
+++ UserBaseWebApplication/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace UserBaseWebApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "CourseTs", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ UserBaseWebApplication/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserBaseWebApplication.Models;
+
+namespace UserBaseWebApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        UsersBaseAppEntities dbobj = new UsersBaseAppEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User objx)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                using (UsersBaseAppEntities dbobj = new UsersBaseAppEntities())
+                {
+                    var obj = dbobj.Users.Where(x => x.username.Equals(objx.username) && x.password.Equals(objx.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else {
+                        ModelState.AddModelError("", "The UserName or password incorrect");
+                    }
+                }
+                
+            }
+            return View(objx);
+        }
+        public ActionResult Logout() {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
